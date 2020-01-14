eb2be1c28607bd304b1f3cc4c74b84184c5a260b
Maksym Bobryshev <maksym.bobmm@gmail.com>
2020-01-14 15:12:45 +0200

    Add generator

---
+++ lib/generators/gem_exercise_generator.rb
@@ -0,0 +1,14 @@
+require 'rails'
+
+class GemExerciseGenerator < Rails::Generators::Base
+  desc "This generator creates an gem_exercise file at config/initializers"
+
+  source_root File.expand_path("../templates", __FILE__)
+
+  def copy_config_file
+    template 'gem_exercise.rb', 'config/initializers/gem_exercise.rb'
+  end
+end
+
+
+
